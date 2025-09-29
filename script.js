c4216505c8e1ba381b26f1a348e6ee0107337e79
Abhishek Mishra <iabm3568@gmail.com>
2025-09-29 10:47:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  	
 
   //your code here
+
+	// Roman numeral mapping (symbol → value)
+  const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+
+  let result = "";
+
+  // Loop through each value
+  for (let i = 0; i < values.length; i++) {
+    // While the number is still >= value, subtract and add symbol
+    while (num >= values[i]) {
+      result += symbols[i]; // add symbol to result
+      num -= values[i];     // reduce number
+    }
+  }
+
+  return result;
+
+}
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+
+// console.log(convertToRoman(36));
+
+
+
+
+// do not edit below this line
+module.exports = convertToRoman
+
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
